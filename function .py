f1c1813d690d294f4c77322dbb8bd7af82fa783b
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-06-29 16:36:14 +0530

    Add files via upload

---
+++ function .py
@@ -0,0 +1,30 @@
+# '''def number_list(list1,list2):
+#     i=0
+#     while i<len(list1):
+#         if list1[i]%2==0 and list2[i]%2==0:
+#             print(i,"dono even hai")
+#         else:
+#             print(i,"dono even nhi hai")
+#         i=i+1
+# number_list([5,6,18,10,3,75],[6,19,24,12,3,87])'''
+
+
+
+# def menu(day):
+#     if day == "monday":
+#         return "Butter Chicken"
+#     elif day == "tuesday":
+#         return "Mutton Chaap"
+#     else:
+#         return "Chole Bhature"
+#     # print("kiya main print ho paunga")
+
+# day=input("enter the day=")
+# print("kiya main print ho paunga")
+# print(menu(day))
+
+
+def add(x=13,y,z):
+    a=x+y+z
+    print(a)
+add(12,56)
---
+++ maximum.py
@@ -0,0 +1,11 @@
+'''print ("NavGurukul")
+
+def say_hello():
+    print ("Hello!")
+    print ("Aap kaise ho?")
+
+say_hello()
+print ("Python is awesome")
+say_hello()
+print ("Hello…")
+say_hello() '''
