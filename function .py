e96d1c0df09170422c261084aacba9dcce9cab1d
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-01 23:33:09 +0530

    Add files via upload

---
+++ function .py
@@ -0,0 +1,12 @@
+def number_list(list1,list2):
+    i=0
+    while i<len(list1):
+        if list1[i]%2==0 and list2[i]%2==0:
+            print(i,"dono even hai")
+        else:
+            print(i,"dono even nhi hai")
+        i=i+1
+number_list([5,6,18,10,3,75],[6,19,24,12,3,87])
+
+
+
