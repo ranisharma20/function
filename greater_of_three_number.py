04723065ff35c5150cae2b714f6850ae3219a6d3
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-01 23:33:53 +0530

    Add files via upload

---
+++ greater_of_three_number.py
@@ -0,0 +1,11 @@
+def greator():
+    if a>b and a>b:
+        print(a,"a is greator")
+    elif b>c and b>c:
+        print(b,"b is greator")
+    elif c>a and c>a:
+        print(c,"c is greator")
+a=int(input("enter the number ="))
+b=int(input("enter the number ="))
+c=int(input("enter the number ="))
+greator()
