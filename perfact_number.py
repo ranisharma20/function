4cfea12e08548e10845c25688f7791b26e5dc27d
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-01 23:22:27 +0530

    Add files via upload

---
+++ perfact_number.py
@@ -0,0 +1,14 @@
+def perfact():
+    num=int(input("enter the number"))
+    sum=0
+    i=1
+    while i<num:
+        if num%i==0:
+            sum=sum+i
+        i=i+1
+    if num==sum:
+        print(num,"is perfact number")
+    else:
+        print("not perfact number")
+
+    
