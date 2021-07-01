2a697b09a773e1c631cd54105163439fbc5e98d2
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-01 23:34:58 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,11 @@
+def greator():
+    i=1
+    while i<=1000:
+        if i%3==0:
+            print(i,"nav")
+        if i%7==0:
+            print(i,"gurukul")
+        if i%21==0:
+            print(i,"navgurukul")
+        i=i+1
+greator()
