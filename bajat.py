2467b600cc54481993e23b19f5ca3e5fff2638de
ranisharma20 <84247684+ranisharma20@users.noreply.github.com>
2021-07-01 23:25:14 +0530

    Add files via upload

---
+++ bajat.py
@@ -0,0 +1,10 @@
+def function():
+    n=int(input("enter the ng bajat ="))
+    x=5000
+    if n>x:
+        print("bajat se bhar hai")
+    if n<x:
+        print("bajat k andar hai")
+    else:
+        pass
+function()
